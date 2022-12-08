fc763737774e62fad283d10c5e966916a93e0b87
Rene0115 <enere0115@gmail.com>
2022-12-08 09:58:22 +0100

    user controller setup

--- config/db.config.js
+++ config/db.config.js
@@ -8,7 +8,7 @@ import logger from '../app.js';
 dotenv.config();
 
 const database = () => {
-  mongoose.connect(process.env.MONGODB_URI).then((value) => logger.info('database connected')).catch((err) => logger.info(err));
+  mongoose.connect(process.env.DATABASE_URI).then((value) => logger.info('database connected')).catch((err) => logger.info(err));
 };
 
 export default database;
